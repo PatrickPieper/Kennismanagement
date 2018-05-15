493d24c8892c8123b23ccfed1f5c3977dc1fe9f9
Patrick <patrick.pieper@student.stenden.com>
2018-05-15 10:28:56 +0200

    Created new mvc Project

---
+++ La-Game/La-Game/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(La_Game.Startup))]
+namespace La_Game
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
