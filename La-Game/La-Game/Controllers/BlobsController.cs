6b7a5af22021abdc02f5c1d15130fca3170e733c
Patrick <patrick.pieper@student.stenden.com>
2018-05-17 11:55:38 +0200

    Creating containers for storing images and audio

---
+++ La-Game/La-Game/Controllers/BlobsController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace La_Game.Controllers
+{
+    public class BlobsController : Controller
+    {
+        // GET: Blobs
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        private CloudBlobContainer GetCloudBlobContainer()
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+                    CloudConfigurationManager.GetSetting("lagame2_AzureStorageConnectionString"));
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+            CloudBlobContainer container = blobClient.GetContainerReference("test-blob-container");
+            return container;
+        }
+
+        public ActionResult CreateBlobContainer()
+        {
+            CloudBlobContainer container = GetCloudBlobContainer();
+
+            ViewBag.Success = container.CreateIfNotExists();
+            ViewBag.BlobContainerName = container.Name;
+
+            return View();
+        }
+
+        public string UploadBlob()
+        {
+            CloudBlobContainer container = GetCloudBlobContainer();
+            CloudBlockBlob blob = container.GetBlockBlobReference("LaGame");
+            blob.Properties.ContentType = "image/jpg";
+
+            //Use filename from uploaded file here
+            using (var fileStream = System.IO.File.OpenRead(@"c:\Users\ppiep\Downloads\La-Game.jpg"))
+            {
+                blob.UploadFromStream(fileStream);
+            }
+            return "success!";
+        }
+
+        public ActionResult ListBlobs()
+        {
+            CloudBlobContainer container = GetCloudBlobContainer();
+            List<String> blobs = new List<String>();
+            foreach (IListBlobItem item in container.ListBlobs(useFlatBlobListing: true))
+            {
+                if (item.GetType() == typeof(CloudBlockBlob))
+                {
+                    CloudBlockBlob blob = (CloudBlockBlob)item;
+                    blobs.Add(blob.Name + "" + blob.Properties.ContentType);
+                    
+                }
+            }
+
+            return View(blobs);
+        }
+    }
+}
