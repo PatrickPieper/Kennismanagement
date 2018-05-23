ee666be80c72a134a68cd36c4212e9fbca547c34
Stfno <stefano.van.der.perk@student.stenden.com>
2018-05-23 13:47:31 +0200

    Fixed Hidden div & javascript

--- La-Game/La-Game/Scripts/answer.js
+++ La-Game/La-Game/Scripts/answer.js
@@ -1,10 +1,11 @@
 ﻿var answerType = $('input[name=answerType]');
+
 answerType.change(function () {
     var selected = $('input[name=answerType]:checked');
     if (selected.val() == 'meerkeuze') {
-        $('.createMcAnswer').removeAttr('hidden');
+        document.getElementById("Option").style.display = 'inline';
     } else {
-        $('.createMcAnswer').hide();
+        document.getElementById("Option").style.display = 'none';
     }
 }
 )
