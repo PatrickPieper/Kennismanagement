6f9d7e4b15d8e158b30352b8664ce97316d09c18
bata <bata_emmen@hotmail.com>
2018-05-23 12:10:21 +0200

    opzet views en controller voor de test

---
+++ La-Game/La-Game/Controllers/StudentTestController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net;
+using La_Game.Models;
+
+namespace La_Game.Controllers
+{
+    public class StudentTestController : Controller
+    {
+        private readonly LaGameDBContext db = new LaGameDBContext();
+
+        // GET: StudentTest
+        public ActionResult Index(string participationCode)
+        {
+            if (participationCode == null)            
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+                return View();
+        }
+    }
+}
