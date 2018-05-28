aa1f4294c855327bacc247d88025a62c6b321c86
Patrick <patrick.pieper@student.stenden.com>
2018-05-28 14:32:29 +0200

    Added max of 6 answers and min of 2

--- La-Game/La-Game/Scripts/answer.js
+++ La-Game/La-Game/Scripts/answer.js
@@ -1,4 +1,4 @@
-﻿var count = 1;
+﻿var count = 2;
 console.log(count);
 var answerType = $('input[name=answerType]');
 
@@ -16,25 +16,45 @@ answerType.change(function () {
 
 $('#AddAnswer').click(function (e) {
     e.preventDefault();
-    count++;
+
     console.log(count);
 
-    url = $(this).data('url');
+    if (count < 6) {
+        url = $(this).data('url');
 
-    target = $('#Option');
-    console.log(url);
+        target = $('#Option');
 
-    $.get(url, function (data) {
-        target.append(data);
-    });
+        $.get(url, function (data) {
+            target.append(data);
+        });
+
+        count++;
+    } else {
+        alertbox = $('#alertBox');
+        alertbox.addClass("alert-danger");
+        alertbox.html("<p>The maximum amount of answers is 6!</p>");
+        alertbox.show();
+        window.scrollTo(0,0);
+    }
+    
 }
 );
 
 $(document).on('click', '.deleteAnswer', function (e) {
     e.preventDefault();
-    count--;
-    console.log(count);
-    deleteAnswer($(this));
+    if (count > 2) {
+        count--;
+        deleteAnswer($(this));
+        
+    } else {
+        alertbox = $('#alertBox');
+        alertbox.addClass("alert-danger");
+        alertbox.empty();
+        alertbox.html("<p>Multiple choice questions need at least 2 answers!</p>");
+        alertbox.show();
+        window.scrollTo(0, 0);
+    }
+    
 }
 );
 
