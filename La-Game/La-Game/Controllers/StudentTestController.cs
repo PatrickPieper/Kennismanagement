5bad6eb5a9a617d1851c12ae1bf954db14d7c2e6
bata <bata_emmen@hotmail.com>
2018-06-11 14:34:13 +0200

    modefied the code in the StudentTestController so that attempts are also being saved in the database

--- La-Game/La-Game/Controllers/StudentTestController.cs
+++ La-Game/La-Game/Controllers/StudentTestController.cs
@@ -49,16 +49,27 @@ namespace La_Game.Controllers
                 string endTime = DateTime.Now.ToString("yyyy:MM:dd HH:mm:ss:fff");
                 DateTime startTime = DateTime.ParseExact((string)TempData["startTime"], "yyyy:MM:dd HH:mm:ss:fff", null);
                 int questionListId = ViewBag.questionListData[0].idQuestionList;
+                int questionId = db.AnswerOptions.Find(studentAnswerId).Question_idQuestion;
                 Participant participant = (Participant)TempData["participant"];
                 // when I have the id from participant set it here
+                string questionResultAttempt = "select qr.* from QuestionResult as qr" +
+                                                        " join AnswerOption as ao on qr.AnswerOption_idAnswer = ao.idAnswer" +
+                                                        " where ao.Question_idQuestion =" + questionId +  
+                                                        " and qr.Participant_idParticipant = " + participant.idParticipant +
+                                                        " and qr.QuestionList_idQuestionList = "+ questionListId +
+                                                        " order by qr.attempt";
+                IEnumerable < QuestionResult > questionResults = db.Database.SqlQuery<QuestionResult>(questionResultAttempt);
+               int? lastAttempt = questionResults.Count() != 0 ? questionResults.Last().attempt : null;
+
+
                 QuestionResult questionResult = new QuestionResult()
                 {
                     QuestionList_idQuestionList = questionListId,
                     AnswerOption_idAnswer = studentAnswerId,
                     Participant_idParticipant = participant.idParticipant,
                     startTime = startTime,
-                    endTime = DateTime.ParseExact(endTime, "yyyy:MM:dd HH:mm:ss:fff", null)
-
+                    endTime = DateTime.ParseExact(endTime, "yyyy:MM:dd HH:mm:ss:fff", null),
+                    attempt = lastAttempt.HasValue ? lastAttempt.Value+1 : 1
                 };
                 db.QuestionResults.Add(questionResult);
                 db.SaveChanges();
